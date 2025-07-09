e5f3939e4c137b3c2d3650dab2d0c0e1121639a1
Ethan Maya <92396506+EthanMaya@users.noreply.github.com>
2025-07-09 20:03:41 +0100

    AI code which splits main code across cores. Core 0 is supposed to do canbus, whilst Core 1 does display, so everything lvgl. Core 0 runs rule engine > handle_gear_selection > update_text > lv_snprintf which is lvgl. need to make a queue so that core 0 makes variables which can be taken by core 1 and used in lvgl

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,3 @@
-/*
-########################## open include directory and edit config.h to change values ############################
-*/
 #include "can_rule_engine.h"
 #include "driver/twai.h"
 #include "ui.h"
@@ -11,48 +8,59 @@
 
 #define CAN_TX 44
 #define CAN_RX 43
-//#define CAN_TX 5
-//#define CAN_RX 4
 #define SPEED 1000
 #define HAS_DISPLAY 1
-// #define P 43
-// works   38 44 43
-// doesn't 19
-typedef u_int8_t u8;
-typedef u_int16_t u16;
 
+typedef uint8_t u8;
+typedef uint16_t u16;
 
-void display_update();
+char buf[16];
+CanFrame rxFrame;
+RuleEngine<CanFrame> rule_engine;
+volatile bool update = false;
+
 
 class CompareIdentifier {
   u16 identifier;
-
 public:
-  CompareIdentifier(u16 identifier) : identifier(identifier){};
+  CompareIdentifier(u16 identifier) : identifier(identifier) {}
   bool operator()(const CanFrame &rxFrame) {
     return rxFrame.identifier == identifier;
   }
 };
 
 void handle_gear_selection(const CanFrame &rxFrame);
+void dim_text(bool condition, lv_obj_t *ui_element);
+void update_text(u16 value, lv_obj_t *ui_element);
 
-//
-char buf[16];
-CanFrame rxFrame;
-RuleEngine<CanFrame> rule_engine;
+TaskHandle_t TaskCanReader;
+TaskHandle_t TaskDisplayUpdater;
 
+// Task: Read CAN and run rule engine
+void can_read_task(void *parameter) {
+  while (true) {
+    if (ESP32Can.readFrame(rxFrame, 1000)) {
+      rule_engine.run(rxFrame);
+    }
+    vTaskDelay(1); // Yield
+  }
+}
 
-boolean update = false;
+// Task: Update display when needed
+void display_task(void *parameter) {
+  while (true) {
+    if (update) {
+      lv_timer_handler();  // Refresh the LVGL UI
+      update = false;
+    }
+    vTaskDelay(10 / portTICK_PERIOD_MS);  // Run at ~100 Hz
+  }
+}
 
-
-boolean gear = false;
-
-void setup()
-{
+void setup() {
   Serial.begin(9600);
 
-  auto SUCCESS =
-      ESP32Can.begin(ESP32Can.convertSpeed(SPEED), CAN_TX, CAN_RX, 10, 10);
+  bool SUCCESS = ESP32Can.begin(ESP32Can.convertSpeed(SPEED), CAN_TX, CAN_RX, 10, 10);
   if (SUCCESS) {
     Serial.println("CAN bus started!");
   } else {
@@ -64,51 +72,50 @@ void setup()
 #if (HAS_DISPLAY)
   init_screen();
 #endif
+
+  // Start CAN reader on Core 0
+  xTaskCreatePinnedToCore(
+    can_read_task,          // Task function
+    "CAN Reader",           // Name
+    4096,                   // Stack size
+    NULL,                   // Param
+    1,                      // Priority
+    &TaskCanReader,         // Task handle
+    0                       // Core 0
+  );
+
+  // Start display update on Core 1
+  xTaskCreatePinnedToCore(
+    display_task,
+    "Display Updater",
+    4096,
+    NULL,
+    1,
+    &TaskDisplayUpdater,
+    1 // Core 1
+  );
 }
 
-void loop() 
-{
-  update = false;
-  if (ESP32Can.readFrame(rxFrame, 1000))
-  {
-    //Serial.println("In loop");
-    rule_engine.run(rxFrame);
-  }
-  if (update)
-  {
-    lv_timer_handler();
-    //delay(10);
-  }
+void loop() {
+  // Empty, tasks handle everything
 }
 
-void dim_text(boolean condition, lv_obj_t *ui_element) 
-{
-  if (condition) 
-  {
-    lv_obj_set_style_text_color(ui_element, lv_color_hex(0xFFFFFF), LV_PART_MAIN | LV_STATE_DEFAULT);
-  }
-  else
-  {
-    lv_obj_set_style_text_color(ui_element, lv_color_hex(0x555555), LV_PART_MAIN | LV_STATE_DEFAULT);
-  }
+// ====================== UI helpers ===========================
 
+
+
+void dim_text(bool condition, lv_obj_t *ui_element) {
+  lv_color_t color = condition ? lv_color_hex(0xFFFFFF) : lv_color_hex(0x555555);
+  lv_obj_set_style_text_color(ui_element, color, LV_PART_MAIN | LV_STATE_DEFAULT);
 }
 
-void update_text(u16 value, lv_obj_t *ui_element) 
-{
+void update_text(u16 value, lv_obj_t *ui_element) {
   lv_snprintf(buf, sizeof(buf), "%d", value);
   lv_label_set_text(ui_element, buf);
   update = true;
 }
 
-void handle_gear_selection(const CanFrame &rxFrame) 
-{
-  //ui_egear           Gear  0x470 6 gear selector position enum
-  //ui_egear
-  // 0x470; bits 7; gear position; enum val ??
-  u16 gear_val = rxFrame.data[7];//7 gear
-  //lv_snprintf(buf, sizeof(buf), "%d", gear_val);
+void handle_gear_selection(const CanFrame &rxFrame) {
+  u16 gear_val = rxFrame.data[7];
   update_text(gear_val, ui_egear);
-
 }
-
